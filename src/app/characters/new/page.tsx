824ec043cfc7f766e6c85a932cc8de6faf4b7ae3
ashevelendar <ashevelendar@gmail.com>
2026-09-22 11:00:46 +0100

    Remove unused creation effect import

--- src/app/characters/new/page.tsx
+++ src/app/characters/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Badge, PageHeader, SectionCard } from "../../../components/AppShell";
 import AbilityScoreBuilder, { applyAbilityBonuses, type AbilityScoreMethod } from "../../../components/AbilityScoreBuilder";
